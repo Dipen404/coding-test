9301ab0f9da51911eba7358b1ab7e8036be788bb
Dipen <hamal.dipendra@gmail.com>
2025-04-02 21:09:26 +0545

    bubble sort

--- src/main/java/com/dipen/coding/sorting/BubbleSort.java
+++ src/main/java/com/dipen/coding/sorting/BubbleSort.java
@@ -1,7 +1,10 @@
 package com.dipen.coding.sorting;
 
+import java.util.stream.Collectors;
+
 public class BubbleSort {
     public static void main(String[] args) {
         System.out.println("working");
+        Collectors.toList()
     }
 }
