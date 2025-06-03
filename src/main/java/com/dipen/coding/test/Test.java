b20376db7d0fa1c64845362ab8d6ce1735727f85
Dipen <hamal.dipendra@gmail.com>
2025-06-03 20:36:55 +0545

    test

--- src/main/java/com/dipen/coding/test/Test.java
+++ src/main/java/com/dipen/coding/test/Test.java
@@ -3,39 +3,42 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.lang.reflect.Array;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Test{
-    public static void main(String[] args) {
-        System.out.println("hello java");
-    }
-}
-
-
-
- class CSVReader {
-    public static void main(String[] args) {
-        String filePath = "D:/member_entry.csv";  // Replace with your CSV file path
-        System.out.println("file path"+filePath);
-        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                // Split the line by commas
-                String[] values = line.split(",");
-
-                // Process the values (this just prints them)
-                for (String value : values) {
-                    System.out.print(value + " ");
+    public static int maxSubStringLength(String string){
+        int maxLength=0;
+        int len=string.length();
+        for(int i=0;i<len;i++){
+            StringBuilder builder=new StringBuilder();
+            for(int j=i;j<len;j++){
+                if(builder.indexOf(String.valueOf(string.charAt(j)))!=-1){
+                    break;
                 }
-                System.out.println(); // Newline after each row
+                builder.append(string.charAt(j));
+                maxLength=Math.max(maxLength,builder.length());
             }
-        } catch (IOException e) {
-            e.printStackTrace();
         }
+        return maxLength;
+    }
+    public static int maxLength(String string){
+        int maxLength=0;
+        int len=string.length();
+        Map<Character,Integer> seen=new HashMap<>();
+        for(int end=0,start=0;end<len;end++){
+            if(seen.containsKey(string.charAt(end))){
+                start=Math.max(start,seen.get(string.charAt(end))+1);
+            }
+            maxLength=Math.max(maxLength,end-start+1);
+            seen.put(string.charAt(end),end);
+        }
+        return maxLength;
+    }
+    public static void main(String[] args) {
+        System.out.println("hello java hello");
+        System.out.println(Test.maxLength("abba"));
     }
 }
+
 
