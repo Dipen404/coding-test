d422846cb5a3e114ca5298cc337a3072c46676c6
Dipen <hamal.dipendra@gmail.com>
2025-06-16 21:12:04 +0545

    changes

--- src/main/java/com/dipen/coding/array/ArrayPrograms.java
+++ src/main/java/com/dipen/coding/array/ArrayPrograms.java
@@ -36,6 +36,45 @@ public class ArrayPrograms {
         }
         return mergedArray;
     }
+    public static int [] removeDuplicate(int [] array){
+        int [] original=array;
+        int length=original.length;
+        int [] result=new int [length];
+        int count=0;
+        for(int i=0;i<length;i++){
+            boolean isDuplicate=false;
+            for(int j=0;j<count;j++){
+                if(original[i]==result[j]){
+                    isDuplicate=true;
+                    break;
+                }
+            }
+            if(!isDuplicate){
+                result[count++]=original[i];
+            }
+        }
+        int [] unique=new int [count];
+        for(int i=0;i<count;i++){
+            unique[i]=result[i];
+        }
+        return unique;
+    }
+    public static int [] removeDupOptimized(int [] array){
+        Arrays.sort(array);
+        int [] temp=new int [array.length];
+        int count=0;
+        temp[count++]=array[0];
+        for(int i=1;i<array.length;i++){
+            if(array[i]!=array[i-1]){
+                temp[count++]=array[i];
+            }
+        }
+        int [] unique=new int [count];
+        for(int i=0;i<count;i++){
+            unique[i]=temp[i];
+        }
+        return unique;
+    }
     public static void main(String[] args) {
         int [] numbers={1,2,3,4,5};
         System.out.println("sum of array: "+ ArrayPrograms.sumOfElements(numbers));
@@ -43,5 +82,8 @@ public class ArrayPrograms {
         int [] first={1,2,3,4};
         int [] second={5,6,7,8};
         System.out.println("merged array: "+Arrays.toString(ArrayPrograms.mergeArray(first,second)));
+        int [] duplicateNums={1,1,1,3,4,5,6,1,6,6};
+        System.out.println(("unique: "+Arrays.toString(ArrayPrograms.removeDuplicate(duplicateNums))));
+        System.out.println(("unique: "+Arrays.toString(ArrayPrograms.removeDupOptimized(duplicateNums))));
     }
 }
